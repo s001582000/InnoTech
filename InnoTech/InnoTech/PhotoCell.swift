d3a96117188f1b1143c84677932704c3d1e9b9eb
xuan <xuan@zoaks.com>
2020-08-21 20:09:25 +0800

    新增tableView

---
+++ InnoTech/InnoTech/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  InnoTech
+//
+//  Created by 梁雅軒 on 2020/8/21.
+//  Copyright © 2020 梁雅軒. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- InnoTech/InnoTech/ViewController.swift
+++ InnoTech/InnoTech/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var arrData = [PhotoInfo]()
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let nib = UINib(nibName: "PhotoCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "PhotoCell")
     }
-
-
 }
 
+extension ViewController : UITableViewDataSource , UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        return cell
+    }
+}
+
+class PhotoInfo {
+    var albumId:String!
+    var id:String!
+    var title:String!
+    var url:String!
+    var thumbnailUrl:String!
+}
