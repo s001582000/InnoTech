92eb57335d29c89bce9f9f4c89d2135f58abf8cc
xuan <xuan@zoaks.com>
2020-08-21 19:55:16 +0800

    init

---
+++ InnoTech/InnoTech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InnoTech
+//
+//  Created by 梁雅軒 on 2020/8/21.
+//  Copyright © 2020 梁雅軒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
