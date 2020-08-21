0f8367118b57c971757a6425241d31cdc47fef06
xuan <xuan@zoaks.com>
2020-08-21 20:17:07 +0800

    新增第三方套件 SDWebImage Alamofire BaseJson4

---
+++ InnoTech/InnoTech/APIManager.swift
@@ -0,0 +1,17 @@
+//
+//  APIManager.swift
+//  InnoTech
+//
+//  Created by 梁雅軒 on 2020/8/21.
+//  Copyright © 2020 梁雅軒. All rights reserved.
+//
+
+import Foundation
+
+class APIManager {
+    static let share = APIManager()
+    func loadData() {
+        let url = "https://jsonplaceholder.typicode.com/photos"
+        
+    }
+}
